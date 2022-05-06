b3f61bd199b523daeda6d54065be65bcf27a7958
Rich Tabor <hi@richtabor.com>
2022-05-06 15:29:04 -0400

    Version bump to 0.2.0

--- post-modified-time-block.php
+++ post-modified-time-block.php
@@ -5,7 +5,7 @@
  * Plugin URI:        https://richtabor.com/post-modified-time-block?utm_source=wp-plugins&utm_medium=post-modified-time-block&utm_campaign=plugin-uri
  * Requires at least: 5.7
  * Requires PHP:      7.0
- * Version:           0.1.1
+ * Version:           0.2.0
  * Author:            RichTabor.com
  * Author URI:        https://richtabor.com/?utm_source=wp-plugins&utm_medium=post-modified-time-block&utm_campaign=author-uri
  * License:           GPL-2.0-or-later
--- readme.txt
+++ readme.txt
@@ -2,7 +2,7 @@
 Contributors:      richtabor
 Tags:              gutenberg, block, blocks, update, posts
 Tested up to:      6.0
-Stable tag:        0.1.1
+Stable tag:        0.2.0
 License:           GPL-2.0-or-later
 License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 
